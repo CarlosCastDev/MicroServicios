7ca4e243b4e22df11947cb753951498300973942
CarlosCastilloDeveloper <CarlosCastilloDeveloper@gmail.com>
2022-04-20 14:15:39 -0500

    archivo de configuracion eureka

--- servicio-registro/src/main/java/com/carloscastillodeveloper/tienda/registro/ServicioRegistroApplication.java
+++ servicio-registro/src/main/java/com/carloscastillodeveloper/tienda/registro/ServicioRegistroApplication.java
@@ -2,8 +2,10 @@ package com.carloscastillodeveloper.tienda.registro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ServicioRegistroApplication {
 
 	public static void main(String[] args) {
