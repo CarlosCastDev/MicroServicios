c7e8b0c6d8b293aa018393f4c8e650a0140b4ec3
CarlosCastilloDeveloper <CarlosCastilloDeveloper@gmail.com>
2022-04-20 14:07:14 -0500

    se agrega proyecto registro con eureka serv

---
+++ servicio-registro/src/main/java/com/carloscastillodeveloper/tienda/registro/ServicioRegistroApplication.java
@@ -0,0 +1,13 @@
+package com.carloscastillodeveloper.tienda.registro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioRegistroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioRegistroApplication.class, args);
+	}
+
+}
---
+++ servicio-registro/src/test/java/com/carloscastillodeveloper/tienda/registro/ServicioRegistroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carloscastillodeveloper.tienda.registro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioRegistroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
